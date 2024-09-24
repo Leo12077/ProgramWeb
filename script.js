fd0b0e1b948328f459d8efa0b7e9237991b45f90
Leo12077 <lolverag2200@alumno.ipn.mx>
2024-09-24 17:58:41 -0600

    ProWeb

---
+++ script.js
@@ -0,0 +1,34 @@
+// Clase 24/09/2024
+// Nombre: Olvera Gil Leonel
+
+//comentario en 1 sola linea
+/*Comentario en multiples
+lineas*/
+
+//Variables
+/*
+Las variables guardan informacion que puedes reutilizar
+*/ 
+
+//Declaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor unicamente para un bloque continuo
+const PI = 3.1416; 
+var edad = 25; // Lo mismo que let pero se puede utilizar en cualuier parte del codigo 
+
+//operadores
+let a = 5;
+let b = 2;
+let sume = a + b; //7
+let resta = a - b; //3
+let multiplicacion = a * b; //10
+let division = a / b //2.5
+
+//operadores de comparacion
+
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
+
+alert("Hola. clase 5IV7 aña js externo");
