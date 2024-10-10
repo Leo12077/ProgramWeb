89f532f9055565045f129f062b0fcd0af13bdce0
Leo12077 <lolverag2200@alumno.ipn.mx>
2024-10-10 16:46:01 -0600

    Cambio

---
+++ 10102024/script.js
@@ -0,0 +1,18 @@
+// Comentario en una linea
+/*
+Comentario multi
+lineas
+*/
+// Declaramos nuestra funcion
+function hello(){
+    return 'hola js'
+}
+
+console.log(hello());
+
+// Investigar diferentes usos de funciones en JS y ejemplificar cada una de ellas
+
+//con uso de parametros, funcion dentro de otra funcion
+
+//
+
