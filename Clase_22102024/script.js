74821e7c3e8000c696a1d3428f48ab56008cbef6
Leo12077 <lolverag2200@alumno.ipn.mx>
2024-10-22 17:43:54 -0600

    Cambio

---
+++ Clase_22102024/script.js
@@ -0,0 +1,24 @@
+//Funcion normal
+function hola(a,b){
+    return a+b;
+}
+
+console.log(hola(3,5));
+
+
+
+//Convertir a funcion flecha
+
+const suma = (a,b) => {
+    return a+b;
+}
+console.log(suma(3,5));
+
+//03 simplificar 
+const suma2 = (a,b) => a+b;
+console.log(suma2(5,5));
+
+//04 solo un parametro
+const cuadrado = x => x*x;
+console.log(cuadrado(5));
+
