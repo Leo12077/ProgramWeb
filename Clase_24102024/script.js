3e29361e94876a010e9498b2272a5d18fba659e7
Leo12077 <lolverag2200@alumno.ipn.mx>
2024-10-24 16:38:20 -0600

    Cambio

---
+++ Clase_24102024/script.js
@@ -0,0 +1,51 @@
+/*WebCode
+Arreglos
+let array = ["dato1",1,Boolean];
+console.log("Tamaño array: " + array.length);
+array.push("Nuevo valor");
+console.log(array[3]);
+console.log("Tamaño array: " + array.length);
+console.log(array[3]);
+
+5*1 = 5 0
+5*2
+5*3
+5*4
+5*5
+5*6
+Fin del arreglo valor de la posicion = 30
+
+let tabla = [];
+
+for(i=1 ; i<=10; i++){
+    if (tabla.length <= 5){
+        tabla.push(5*i);
+        console.log("5*" + i + "=" + tabla[i-1]);
+    }else{
+        console.log("Fin del arreglo valor de la posicion = " + tabla[5]);
+    }
+}
+*/
+
+/*
+//Eliminar elementos de un array
+let array = ["dato1","dato2","dato3"];
+array.push("dato4");
+console.log(array[3]);
+
+//Eliminar
+array.splice(1,1);
+console.log(array);
+*/
+
+//Practica eliminar
+
+const input = document.getElementById(input_1);
+
+let tareas = [];
+
+
+
+
+
+
